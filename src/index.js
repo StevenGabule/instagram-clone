3b3454fb1cb56b3b6b903538a4cde6f75096119e
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-05-23 15:54:50 +0800

    learn about hasura - real time database using postgres

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {render} from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import {BrowserRouter} from "react-router-dom";
+import {Auth0Provider} from "./auth/react-auth0-wrapper";
+import config from "./auth/auth_config.json";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = appState => {
+    window.history.replaceState(
+        {},
+        document.title,
+        appState && appState.targetUrl
+            ? appState.targetUrl
+            : window.location.pathname
+    );
+};
+
+render(
+    <BrowserRouter>
+        <Auth0Provider
+            domain={config.domain}
+            client_id={config.clientId}
+            audience={config.audience}
+            redirect_uri={config.redirect_uri}
+            onRedirectCallback={onRedirectCallback}>
+            <App/>
+        </Auth0Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
